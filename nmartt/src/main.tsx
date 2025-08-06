8cfb585ae402c631b550c8ed120274f3defbc2aa
femzi <108510189+femzi@users.noreply.github.com>
2025-08-06 22:05:42 +0100

    Hero

--- nmartt/src/main.tsx
+++ nmartt/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header.tsx'
+import Hero from './Hero.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
+    <Hero/>
   </StrictMode>,
 )
